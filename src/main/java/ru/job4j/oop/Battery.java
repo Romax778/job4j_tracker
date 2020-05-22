1aaef7a3cacb98a5fd6c467ecc6fae9a5086433e
romax778 <romax777f@gmail.com>
2020-05-22 09:29:22 +0100

    job4j_tracker

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -11,12 +11,15 @@ public class Battery {
         this.load = this.load + another.load;
         another.load = 0;
 
+
+
+
     }
     public static void main(String[]args){
         Battery first = new Battery(5);
         Battery second = new Battery(2);
         System.out.println("first : " + first.load + ". second : " + second.load);
-first.exchange(second);
+second.exchange(first);
         System.out.println("first : " + first.load + ". second : " + second.load);
     }
 }
