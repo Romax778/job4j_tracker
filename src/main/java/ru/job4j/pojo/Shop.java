8d7876b47e3a1c1c84acf47ed1b6ab6fd8221ac0
romax778 <romax777f@gmail.com>
2020-06-05 06:20:18 +0100

    job4j_tracker

--- src/main/java/ru/job4j/pojo/Shop.java
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -1,8 +1,12 @@
 package ru.job4j.pojo;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Shop {
-    public Product[] delete(Product[] products, int index) {
+    public Product[]delete(Product[] products, int index) {
         for (int i = index; i < products.length-1; i++) {
+            Product product = products[i];
             products[i] = products[i + 1];
 
         }
@@ -12,10 +16,20 @@ public class Shop {
 
 
     public static void main(String[] args) {
-        Product products[] = new Product[5];
+Shop shop = new Shop();
+        Product products[] = new Product[4];
         products[0] = new Product("Milk", 10);
         products[1] = new Product("Bread", 4);
-        products[2] = new Product("Egg", 19);
+        products[2] = null;
+        products[3] = new Product("Egg", 19);
+products = shop.delete(products,2);
+for (int i = 0; i < products.length ; i++){
+    if(products[i]!=null){
+        System.out.println(products[i].getName());
+    }else{
+        System.out.println("empty");
+    }
+}
 
 
 
