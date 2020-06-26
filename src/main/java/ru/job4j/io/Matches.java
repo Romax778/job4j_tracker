5aebad528bb474e457306b97fb84d79071791a37
romax778 <romax777f@gmail.com>
2020-06-26 06:19:53 +0100

    job4j_tracker

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Matches {
     public static void main(String[] args) {
         int matches = 11;
-        boolean run = true;
+        boolean igrok_1 = true;
+        boolean igrok_2 = false;
         Scanner inputPlayer1 = new Scanner(System.in);
         Scanner inputPlayer2 = new Scanner(System.in);
         while (true) {
@@ -13,18 +14,26 @@ public class Matches {
 
 
             int chois1 = Integer.valueOf(inputPlayer1.nextLine());
+            System.out.println(" Igrok_1 zakonchil");
             matches = matches - chois1;
             System.out.println(matches + " matches left");
             System.out.println("Player2 take matches from 1 to 3");
             int chois2 = Integer.valueOf(inputPlayer2.nextLine());
+            System.out.println("Igrok_2 zakonchil");
             matches = matches - chois2;
             System.out.println(matches + " matches left");
 
-            if (matches == 0) {
-                System.out.println("Game Over");
-                run = false;
+            if (matches ==0  & igrok_1) {
+                System.out.println("Game Over! Igrok_1 pobedil");
+
                 break;
             }
+            else if(chois1 ==0 & igrok_2){
+                System.out.println("Game over! Igrok_2 pobedil2");
+
+                break;
+            }
+
         }
     }
 }
