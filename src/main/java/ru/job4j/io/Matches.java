1a60f12e9ff9e264192b5a7a4d33657a2869509f
romax778 <romax777f@gmail.com>
2020-07-01 21:03:28 +0100

    job4j_tracker

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -1,39 +1,38 @@
 package ru.job4j.io;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Matches {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Random rnd = new Random();
+
         int matches = 11;
-        boolean igrok_1 = true;
-        boolean igrok_2 = false;
-        Scanner inputPlayer1 = new Scanner(System.in);
-        Scanner inputPlayer2 = new Scanner(System.in);
-        while (true) {
-            System.out.println("Player1 take matches from 1 to 3");
+        boolean repeatGame = true;
+        int playerNumber = rnd.nextInt(2) + 1; // 0...1 +1
+        int userChoice;
 
 
-            int chois1 = Integer.valueOf(inputPlayer1.nextLine());
-            System.out.println(" Igrok_1 zakonchil");
-            matches = matches - chois1;
-            System.out.println(matches + " matches left");
-            System.out.println("Player2 take matches from 1 to 3");
-            int chois2 = Integer.valueOf(inputPlayer2.nextLine());
-            System.out.println("Igrok_2 zakonchil");
-            matches = matches - chois2;
-            System.out.println(matches + " matches left");
+        do {
+            System.out.print("Player " + playerNumber + " take matches from 1 to 3 : ");
+            do {
+                userChoice = input.nextInt();
+                if (userChoice < 1 || userChoice > 3 || userChoice>matches) System.out.println("Wrong amount of matches, try again!!!");
+                else break;
+            } while (true);
 
-            if (matches ==0  & igrok_1) {
-                System.out.println("Game Over! Igrok_1 pobedil");
+            matches -= userChoice;
 
-                break;
-            }
-            else if(matches ==0 & igrok_2){
-                System.out.println("Game over! Igrok_2 pobedil2");
-
-                break;
+            if (matches == 0) {
+                System.out.println("Game Over! Player " + playerNumber + " win!!!");
+                repeatGame = false;
+            } else {
+                System.out.println(matches + " matches left");
             }
 
-        }
+            playerNumber = playerNumber==1?2:1;
+
+        } while (repeatGame);
     }
 }
