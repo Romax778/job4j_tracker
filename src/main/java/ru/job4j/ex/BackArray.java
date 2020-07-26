4ff73caf8950f08f8232c24f092de1cbe733bf56
romax778 <romax777f@gmail.com>
2020-07-26 11:51:21 +0100

    job4j_tracker

--- src/main/java/ru/job4j/ex/BackArray.java
+++ src/main/java/ru/job4j/ex/BackArray.java
@@ -6,10 +6,10 @@ public class BackArray {
     public static void main(String[] args) {
         String[] names = {"Petr", "Ivan", "Nik", "Vasya"};
         int middle = names.length / 2;
-        for (int index = 0; index <= middle; index++) {
-            String temp = names[index];
-            names[index] = names[names.length - 1];
-            names[names.length - 1] = temp;
+        for (int i = 0; i <= middle; i++) {
+            String temp = names[i];
+            names[i] = names[names.length-1 - i ];
+            names[names.length-1 - i ] = temp;
         }
         System.out.println(Arrays.toString(names));
     }
