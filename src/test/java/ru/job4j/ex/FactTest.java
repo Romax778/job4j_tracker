fb592514dd753dd2ad3f8250dfd39371a5f7ba70
romax778 <romax777f@gmail.com>
2020-07-26 21:41:22 +0100

    job4j_tracker

---
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -0,0 +1,9 @@
+package ru.job4j.ex;
+import org.junit.Test;
+
+public class FactTest {
+    @Test(expected = IllegalArgumentException.class)
+    public void whenLessThenN() {
+        Fact.calc(-10);
+    }
+}
