4d8227e71a9e08646adba2fa77073743540cb168
romax778 <romax777f@gmail.com>
2020-09-24 05:15:51 +0100

    job4j_tracker

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -7,13 +7,17 @@ public class FindEl {
         for (int i = 0 ; i < value.length; i++) {
             if (value[i].equals(key)) {
 
-
-
-
-
-
-                return rsl;
+                return i;
             }
         }throw new ElementNotFoundExeption("Element " + key + " Not found");
+    }
+    public static void main(String[]args)  {
+        String[]arr = {"Kyiv","Riga","Lviv"};
+        try {
+            int res = indexOf(arr,"Lviv");
+            System.out.println(res);
+        } catch (ElementNotFoundExeption elementNotFoundExeption) {
+            elementNotFoundExeption.printStackTrace();
+        }
     }
 }
